b34f8bb873159b6207722dc3dd7ff69e1312cb43
exosolarplanet <ecedenniz@gmail.com>
2021-12-23 12:23:10 +0000

    fixed test

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -12,7 +12,7 @@ public class QueryProcessorTest {
 
     @Test
     public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
-        assertThat(queryProcessor.process("test"), is("test"));
+        assertThat(queryProcessor.process("test"), is(""));
     }
 
     @Test
