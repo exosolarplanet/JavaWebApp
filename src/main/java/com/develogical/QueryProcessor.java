300bcaf1e4bd1f6eeac51904815210687b8ad2ba
exosolarplanet <ecedenniz@gmail.com>
2021-12-23 14:04:26 +0000

    added query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,12 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+
+        if (query.toLowerCase().contains("marlowe")){
+            return "Christopher Marlowe, (baptised 26 February 1564 – 30 May 1593), " +
+                    "was an English playwright, poet and translator of the Elizabethan era." +
+                    " Marlowe is among the most famous of the Elizabethan playwrights.";
+        }
         return "";
     }
 }
